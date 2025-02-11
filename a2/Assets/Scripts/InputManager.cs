82d31a232a5c366206c8c57050884d3a1178a631
rin! <phurinmeng@gmail.com>
2025-02-11 14:27:38 -0800

    player movement

--- a2/Assets/Scripts/InputManager.cs
+++ a2/Assets/Scripts/InputManager.cs
@@ -3,21 +3,31 @@ using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-
     public UnityEvent OnSpacePressed = new UnityEvent();
+
+    public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             OnSpacePressed?.Invoke();
         }
-        
+        Vector2 input = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            input += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input += Vector2.right;
+        }
+        OnMove?.Invoke(input);
     }
 }
---
+++ a2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float speed;
+    private Rigidbody rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        inputManager.OnMove.AddListener(MovePlayer);
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void MovePlayer(Vector2 direction)
+    {
+        Vector3 moveDirection = new(direction.x, 0f, direction.y);
+        rb.AddForce(speed * moveDirection);
+    }
+}
